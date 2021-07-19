ed5c52feb1e4a1ffce8ca707e210dabd7aace196
syncdai <80217508+syncdai@users.noreply.github.com>
2021-07-19 02:35:16 +0000

    Add files via upload

--- starter_file/train.py
+++ starter_file/train.py
@@ -13,7 +13,7 @@ pdata = pdata[pdata["Year"]>2009]
 topTenFeatures = ["G1", "G2", "Seed1", "Seed2", "PF1", "PF2", "3P%1", "3P%2", "TOV1", "TOV2", "winner"]
 x = pdata[topTenFeatures]
 y = x.pop("winner").apply(lambda s: 1 if s == "Team2" else 0)
-cv = KFold(n_splits=10, shuffle=False)
+cv = KFold(n_splits=10, shuffle=True, random_state=42)
 
 run = Run.get_context()
 
@@ -21,20 +21,27 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--C", type=float, default=1.0, help="inverse of regularization strength")
-    parser.add_argument("--penalty", type=str, default="l1", help="norm used in penalization")
-    parser.add_argument("--multi_class", type=str, default="ovr", help="multinomial loss fit")
+    parser.add_argument("--C", type=float, default=2.0, help="inverse of regularization strength")
+    parser.add_argument("--max_iter", type=int, default=500, help="max iterations for convergence")
+    parser.add_argument("--penalty", type=str, default="l2", help="norm used in penalization")
+    parser.add_argument("--solver", type=str, default="saga", help="algorithm name")
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Penalization Norm:", np.str(args.penalty))
-    run.log("Multinomial Fit:", np.str(args.multi_class))
+    run.log("Regularization:", np.float(args.C))
+    run.log("Max Iterations:", np.str(args.max_iter))
+    run.log("Penalty:", np.str(args.penalty))
+    run.log("Solver:", np.str(args.solver))
 
-    model = LogisticRegression(C=args.C, penalty=args.penalty, multi_class=args.multi_class, solver="saga")
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter, penalty=args.penalty, solver=args.solver)
 
     scores = cross_val_score(model, x, y, scoring="accuracy", cv=cv, n_jobs=-1)
+    run.log("Scores", scores)
     run.log("Accuracy", np.mean(scores))
+    
+    # train model on all samples and save it
+    model.fit(x, y)
+    joblib.dump(value=model, filename="./outputs/hd_model.pkl")
 
 if __name__ == '__main__':
     main()
